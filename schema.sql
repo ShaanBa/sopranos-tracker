921a250072d69b555cc3555177be1f020f3b1bd0
ShaanBa <162068352+ShaanBa@users.noreply.github.com>
2026-03-16 18:55:16 -0500

    feat: added Crew tablem with FAMID FK

--- schema.sql
+++ schema.sql
@@ -1,6 +1,15 @@
 -- schema for DB starting March 16
 CREATE TABLE Family (
-    FamilyID INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     Name VARCHAR(255) NOT NULL,
     BaseCity VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Crew (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    FamilyID INT NOT NULL,
+    CONSTRAINT fk_Family
+    FOREIGN KEY (FamilyID)  
+    REFERENCES Family(id)
 );
