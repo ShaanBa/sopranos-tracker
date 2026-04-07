9ee50170ed2edbf3c3ceae7b5f6213eba5c8560d
ShaanBa <162068352+ShaanBa@users.noreply.github.com>
2026-04-07 15:34:59 -0500

    feat: Initiated MVC dashboard for Project

---
+++ SopranosDashboard/Models/DomainModels.cs
@@ -0,0 +1,56 @@
+public class Family
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string BaseCity { get; set; }
+}
+
+public class Crew
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Capo { get; set; }
+    public int FamilyID { get; set; }
+
+}
+
+public class Mobster
+{
+    public int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public string? Moniker { get; set; }
+    public required string Rank { get; set; }
+    public required string Status { get; set; }
+    public int CrewID { get; set; }
+
+}
+
+public class KnownAssociate
+{
+    public int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string RelationType { get; set; }
+    public int MobsterId { get; set; }
+
+}
+
+public class BusinessFront
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Industry { get; set; }
+    public required string StreetAddress { get; set; }
+}
+
+public class CollectionLog
+{
+    public int Id { get; set; }
+    public float Amount { get; set; }
+    public DateTime DateCollected { get; set; }
+    public int MobsterID {get; set; }
+    public int BusinessID { get; set; }
+    
+}
+
---
+++ SopranosDashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SopranosDashboard.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
