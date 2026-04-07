9b6d74983c30fe216cf798f4a5f2ad9d3fbc81e7
ShaanBa <162068352+ShaanBa@users.noreply.github.com>
2026-04-06 21:42:31 -0500

    feat: add ADO.NET MySQL connection test and data mapping

--- Program.cs
+++ Program.cs
@@ -12,6 +12,34 @@ using (var connection = new MySqlConnection(connstr))
     try // anything can happen working on a connection so do it in a try catch with
     {
         connection.Open();  // maybe this is like in psycopg2 the cursor that the connection uses to do work
+        // sample sql query 
+        string sql = "SELECT * FROM KnownAssociate";
+
+        // here i am thinking we create a new command called command that will be the sql query  executed by the connection 
+        using (MySqlCommand cmd = new MySqlCommand(sql, connection))
+        {   
+            // we then create this reader object that will read the result of the command 
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                // i guess here our reader object will begin reading 
+                while (reader.Read())
+                {
+                    // for each returned tuple we do some work so here i am printing it 
+                    KnownAssociate associate = new KnownAssociate
+                    {
+                        Id = reader.GetInt32("Id"),
+                        FirstName = reader.GetString("FirstName"),
+                        LastName = reader.GetString("LastName"),
+                        RelationType = reader.GetString("RelationType"),
+                        MobsterId = reader.GetInt32("MobsterId")
+                    };
+
+                    Console.WriteLine($"Id: {associate.Id}, Name: {associate.FirstName} {associate.LastName}, Relation: {associate.RelationType}, MobsterId: {associate.MobsterId}");
+                    
+                }
+            }
+        }
+
         Console.WriteLine("Connected!"); // write some random shtiff 
     }
 
